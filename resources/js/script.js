2c00515c1b466cd9b3d2c7f5b61ceef69568af49
anscmc <an_sc_mc@yahoo.com>
2020-05-25 13:19:10 -0500

    mobile update

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 2000;
 
 });
-
 function toggleMenu(){
-    $('#mobileMenu').fadeToggle(500);
+    $('#mobileMenu').fadeToggle(200);
 }
